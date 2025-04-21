334f003e68ec915877c713508dfe3376a0124ef3
NicoAriestP <nicoariestputra@gmail.com>
2025-04-21 21:26:37 +0700

    add student class migration

---
+++ database/migrations/2024_05_15_170409_create_student_classes_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_classes', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('created_by')
+                ->nullable()
+                ->constrained('users')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            $table->foreignId('updated_by')
+                ->nullable()
+                ->constrained('users')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            $table->foreignId('year_id')
+                ->nullable()
+                ->constrained('years')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            $table->string('name', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_classes');
+    }
+};
