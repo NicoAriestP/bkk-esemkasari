6cce0e84b01ddf40f65f65cbe3f95cf359c8d1bd
NicoAriestP <nicoariestputra@gmail.com>
2025-04-22 23:46:35 +0700

    feat:question option factory

---
+++ database/factories/QuestionOptionFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\QuestionOption>
+ */
+class QuestionOptionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'question_id' => fake()->unique()->numberBetween(1, 10),
+            'option_label' => fake()->word(),
+        ];
+    }
+}
