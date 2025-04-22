0a62fa8aa8f3b3a32ff36f113d26cdbf2ff48dbb
NicoAriestP <nicoariestputra@gmail.com>
2025-04-22 22:48:40 +0700

    make phone column nullable in users migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
                 ->onDelete('restrict');
 
             $table->string('name', 50);
-            $table->string('phone', 20);
+            $table->string('phone', 20)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
