7ceef921a359992592a563a23e09d1be8865f853
NicoAriestP <nicoariestputra@gmail.com>
2025-04-22 23:43:20 +0700

    feat:questionnaire factory

---
+++ database/factories/QuestionnaireFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Questionnaire>
+ */
+class QuestionnaireFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(3),
+            'description' => fake()->paragraph(3),
+            'type' => fake()->randomElement(['single', 'multiple']),
+            'due_at' => fake()->dateTimeBetween('+1 week', '+2 week'),
+        ];
+    }
+}
