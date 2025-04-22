234058e8d292ff677b3f690303f94c89e2ca3f3c
NicoAriestP <nicoariestputra@gmail.com>
2025-04-22 22:50:17 +0700

    feat: partner factory

---
+++ database/factories/PartnerFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Partner>
+ */
+class PartnerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->company(),
+            'phone' => fake()->unique()->phoneNumber(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => Hash::make('password'),
+            'address' => fake()->address(),
+        ];
+    }
+}
