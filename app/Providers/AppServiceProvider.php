820661364d69e35cdd10bbd38f222a6e13136ba0
NicoAriestP <nicoariestputra@gmail.com>
2025-04-28 21:00:15 +0700

    call observer files in app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        //Observers
+        \App\Models\Announcement::observe(\App\Observers\AnnouncementObserver::class);
+        \App\Models\User::observe(\App\Observers\UserObserver::class);
+        \App\Models\Student::observe(\App\Observers\StudentObserver::class);
+        \App\Models\StudentClass::observe(\App\Observers\StudentClassObserver::class);
+        \App\Models\Year::observe(\App\Observers\YearObserver::class);
+
         Inertia::share([
             'auth' => fn () => [
                 'user' => Auth::user(),
