840b90272cabcc960996d2d86ac1b823f86fec00
NicoAriestP <nicoariestputra@gmail.com>
2025-04-29 16:05:05 +0700

    feat: implement featured file on announcement

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -5,14 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\Model\Blameable;
+use App\Traits\HasFeaturedFile;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\Storage;
 
 class Announcement extends Model
 {
-    use HasFactory, Blameable;
+    use HasFactory, Blameable, HasFeaturedFile;
 
     protected $fillable = [
         'title',
         'content',
         'file'
     ];
+
+    protected $useTypeForFileFolderName = true;
+    protected $type = 'announcement';
+
+    protected $appends = [
+        'file_url',
+    ];
+
+    /**
+     * Provide web accessible image url.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    public function fileUrl(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->file
+                ? Storage::disk(config('filesystems.default', 'public'))->url($this->file)
+                : null,
+        );
+    }
 }
