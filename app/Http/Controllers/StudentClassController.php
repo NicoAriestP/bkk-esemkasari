b3ba43f68fd97b1e397ee5b70ff312257e0f31bf
NicoAriestP <nicoariestputra@gmail.com>
2025-05-09 20:25:36 +0700

    feat: crud student class (kelas)

--- app/Http/Controllers/StudentClassController.php
+++ app/Http/Controllers/StudentClassController.php
@@ -8,10 +8,11 @@ use App\Models\StudentClass;
 use App\Http\Requests\StudentClass\CreateStudentClassFormRequest;
 use App\Http\Requests\StudentClass\EditStudentClassFormRequest;
 use App\Actions\StudentClass\StudentClassAction;
+use App\Models\Year;
 
 class StudentClassController extends Controller
 {
-    public function index(Request $request)
+    public function index(Year $year, Request $request)
     {
         $search = $request->input('search', '');
 
@@ -19,48 +20,34 @@ class StudentClassController extends Controller
             ->when($search, function ($query, $search) {
                 $query->where('name', 'like', "%$search%");
             })
+            ->where('year_id', $year->id)
             ->orderBy('created_at', 'desc')
             ->get();
 
         return Inertia::render('studentClass/List', [
             'models' => $studentClasses,
+            'year' => $year,
         ]);
     }
 
-    public function create()
-    {
-        return Inertia::render('studentClass/Form', [
-            'model' => new StudentClass(),
-            'isNewRecord' => true,
-        ]);
-    }
-
-    public function store(CreateStudentClassFormRequest $request, StudentClassAction $action)
+    public function store(Year $year, CreateStudentClassFormRequest $request, StudentClassAction $action)
     {
         $model = $action->save($request);
 
-        return redirect()->route('student-classes.index');
+        return redirect()->route('student-classes.index', $year->id);
     }
 
-    public function edit(StudentClass $model)
-    {
-        return Inertia::render('studentClass/Form', [
-            'model' => $model,
-            'isNewRecord' => false,
-        ]);
-    }
-
-    public function update(EditStudentClassFormRequest $request, StudentClass $model, StudentClassAction $action)
+    public function update(Year $year, EditStudentClassFormRequest $request, StudentClass $model, StudentClassAction $action)
     {
         $action->update($model, $request);
 
-        return redirect()->route('student-classes.index');
+        return redirect()->route('student-classes.index', $year->id);
     }
 
-    public function destroy(StudentClass $model)
+    public function destroy(Year $year, StudentClass $model)
     {
         $model->delete();
 
-        return redirect()->route('student-classes.index');
+        return redirect()->route('student-classes.index', $year->id);
     }
 }
--- resources/js/pages/studentClass/List.vue
+++ resources/js/pages/studentClass/List.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-import { Head, router, useForm } from '@inertiajs/vue3';
 import AppLayout from '@/layouts/AppLayout.vue';
+import { Head, router, useForm } from '@inertiajs/vue3';
 import { useToast } from 'primevue/usetoast';
+import { ref, watch } from 'vue';
 
 import { BreadcrumbItem } from '@/types';
 
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import InputText from "primevue/inputtext";
 import Button from 'primevue/button';
+import Column from 'primevue/column';
+import DataTable from 'primevue/datatable';
 import Dialog from 'primevue/dialog';
+import InputText from 'primevue/inputtext';
 import Toast from 'primevue/toast';
 
 const props = defineProps({
     models: {
         type: Array,
+        required: true,
+    },
+    year: {
+        type: Object,
         required: true,
     },
 });
@@ -26,8 +30,16 @@ const toast = useToast();
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
+        title: 'Angkatan',
+        href: route('years.index'),
+    },
+    {
+        title: props.year.year,
+        href: '#',
+    },
+    {
         title: 'Kelas',
-        href: '/student-classes.index',
+        href: route('student-classes.index', props.year.id),
     },
 ];
 
@@ -36,35 +48,40 @@ const dialogVisible = ref(false);
 
 const form = useForm({
     _method: '',
+    year_id: props.year.id,
     id: null as number | null, // Tambahkan ID untuk edit dan delete
     name: null,
 });
 
 watch(filters, (newValue) => {
-    router.get(route('student-classes.index'), { search: newValue }, {
-        preserveState: true,
-        preserveScroll: true,
-        replace: true,
-    });
+    router.get(
+        route('student-classes.index', props.year.id),
+        { search: newValue },
+        {
+            preserveState: true,
+            preserveScroll: true,
+            replace: true,
+        },
+    );
 });
 
 // Function to open modal for adding data
 const openCreateModal = () => {
-    dialogMode.value = "add";
+    dialogMode.value = 'add';
     form.reset();
     dialogVisible.value = true;
 };
 
 const openEditModal = (studentClass: any) => {
-    dialogMode.value = "edit";
+    dialogMode.value = 'edit';
     Object.assign(form, studentClass);
     dialogVisible.value = true;
 };
 
 // Save changes for add or edit
 const saveStudentClass = () => {
-    if (dialogMode.value === "add") {
-        form._method = "POST";
+    if (dialogMode.value === 'add') {
+        form._method = 'POST';
 
         form.post(route('student-classes.store'), {
             errorBag: 'Kelas',
@@ -87,10 +104,12 @@ const saveStudentClass = () => {
                 });
             },
         });
-    } else if (dialogMode.value === "edit") {
-        form._method = "PUT";
+    } else if (dialogMode.value === 'edit') {
+        form._method = 'PUT';
 
-        form.post(route('student-classes.update', form.id), {
+        const routeIdParam = form.id as number;
+
+        form.post(route('student-classes.update', routeIdParam), {
             errorBag: 'Kelas',
             preserveScroll: true,
             onSuccess: () => {
@@ -141,6 +160,10 @@ const deleteStudentClass = (id: number) => {
         },
     });
 };
+
+const openStudentsPage = (model: any) => {
+    router.get(route('students.index', { year: model.year_id, studentClass: model.id }));
+}
 </script>
 
 <template>
@@ -148,35 +171,60 @@ const deleteStudentClass = (id: number) => {
     <Toast />
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="lg:flex lg:justify-center">
-            <DataTable class="w-full max-w-2xl" :value="props.models" paginator removableSort row-hover :rows="10" :rows-per-page-options="[10, 20, 50, 100]">
-            <template #header>
-                <div class="flex flex-col lg:flex-row lg:justify-between lg:items-center gap-2">
-                    <InputText class="w-full lg:w-72" v-model="filters" placeholder="Search ..." />
-                    <Button label="Tambah" @click="openCreateModal" variant="primary" icon="pi pi-plus" />
-                </div>
-            </template>
-            <template #empty> No data found. </template>
-
-            <Column field="name" sortable header="Nama Kelas"/>
-            <Column header="Aksi" body-style="text-align: right;" header-style="text-align: center !important;" style="width: 10%;">
-                <template #body="slotProps">
-                    <div class="flex justify-center">
-                        <Button class="!text-yellow-500 hover:!text-yellow-600" icon="pi pi-pencil" variant="link" severity="secondary"
-                            @click="openEditModal(slotProps.data)" />
-                        <Button class="!text-red-500 hover:!text-red-600" icon="pi pi-trash" variant="link" severity="danger"
-                            @click="deleteStudentClass(slotProps.data.id)" />
+            <DataTable
+                class="w-full max-w-2xl"
+                :value="props.models"
+                paginator
+                removableSort
+                row-hover
+                :rows="10"
+                :rows-per-page-options="[10, 20, 50, 100]"
+                @rowClick="(event: any) => openStudentsPage(event.data)"
+            >
+                <template #header>
+                    <div class="flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-between">
+                        <InputText class="w-full lg:w-72" v-model="filters" placeholder="Search ..." />
+                        <Button label="Tambah" @click="openCreateModal" variant="primary" icon="pi pi-plus" />
                     </div>
                 </template>
-            </Column>
-        </DataTable>
+                <template #empty> No data found. </template>
+
+                <Column field="name" sortable header="Nama Kelas" />
+                <Column header="Aksi" body-style="text-align: right;" header-style="text-align: center !important;" style="width: 10%">
+                    <template #body="slotProps">
+                        <div class="flex justify-center">
+                            <Button
+                                class="!text-yellow-500 hover:!text-yellow-600"
+                                icon="pi pi-pencil"
+                                variant="link"
+                                severity="secondary"
+                                @click="openEditModal(slotProps.data)"
+                            />
+                            <Button
+                                class="!text-red-500 hover:!text-red-600"
+                                icon="pi pi-trash"
+                                variant="link"
+                                severity="danger"
+                                @click="deleteStudentClass(slotProps.data.id)"
+                            />
+                        </div>
+                    </template>
+                </Column>
+            </DataTable>
         </div>
 
-        <Dialog class="w-[32rem]" :header="dialogMode === 'create' ? 'Tambah Kelas' : 'Ubah Kelas'" v-model:visible="dialogVisible" modal :closable="false">
+        <Dialog
+            class="w-[32rem]"
+            :header="dialogMode === 'create' ? 'Tambah Kelas' : 'Ubah Kelas'"
+            v-model:visible="dialogVisible"
+            modal
+            :closable="false"
+        >
             <form @submit.prevent="saveStudentClass">
-                <div class="flex flex-col gap-2 my-2">
+                <div class="my-2 flex flex-col gap-2">
                     <InputText v-model="form.name" placeholder="Nama Kelas" />
                 </div>
-                <div class="flex justify-end gap-2 my-2">
+                <div class="my-2 flex justify-end gap-2">
                     <Button type="button" icon="pi pi-times" class="p-button-text" label="Batal" @click="dialogVisible = false" />
                     <Button type="submit" icon="pi pi-check" label="Simpan" />
                 </div>
