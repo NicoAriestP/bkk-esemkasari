9fc763766297e1e4badb206b1fecb1bff2b4a549
NicoAriestP <nicoariestputra@gmail.com>
2025-06-03 02:10:19 +0700

    chore: crud user

--- resources/js/pages/user/List.vue
+++ resources/js/pages/user/List.vue
@@ -91,31 +91,33 @@ const createUser = () => {
 };
 
 const updateUser = () => {
-    console.log(form.id);
-    form.put(route('users.update', {
-        model: form.id,
-    }), {
-        errorBag: 'user',
-        preserveScroll: true,
-        onSuccess: () => {
-            toast.add({
-                severity: 'success',
-                summary: 'Sukses',
-                detail: 'Karyawan berhasil diperbarui!',
-                life: 5000,
-            });
-            dialogVisible.value = false;
-            form.reset();
+    form.put(
+        route('users.update', {
+            model: form.id,
+        }),
+        {
+            errorBag: 'user',
+            preserveScroll: true,
+            onSuccess: () => {
+                toast.add({
+                    severity: 'success',
+                    summary: 'Sukses',
+                    detail: 'Karyawan berhasil diperbarui!',
+                    life: 5000,
+                });
+                dialogVisible.value = false;
+                form.reset();
+            },
+            onError: () => {
+                toast.add({
+                    severity: 'error',
+                    summary: 'Error',
+                    detail: 'Terjadi kesalahan saat memperbarui karyawan',
+                    life: 5000,
+                });
+            },
         },
-        onError: () => {
-            toast.add({
-                severity: 'error',
-                summary: 'Error',
-                detail: 'Terjadi kesalahan saat memperbarui karyawan',
-                life: 5000,
-            });
-        },
-    });
+    );
 };
 
 const deleteUser = () => {
@@ -167,6 +169,7 @@ watch(filters, (newValue) => {
 
 <template>
     <Toast />
+
     <Head title="Karyawan" />
     <AppLayout :breadcrumbs="breadcrumbs">
         <DataTable
@@ -220,56 +223,62 @@ watch(filters, (newValue) => {
     </AppLayout>
 
     <Dialog
-            v-model:visible="dialogVisible"
-            modal
-            :header="dialogMode === 'add' ? 'Tambah Karyawan' : 'Ubah Karyawan'"
-            class="w-full max-w-lg sm:max-w-xl md:max-w-2xl"
-        >
-            <div class="flex flex-col gap-y-4 p-2 lg:p-4">
-                <div>
-                    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                        <div>
-                            <label for="name" class="block text-sm font-medium text-foreground">Nama</label>
-                            <InputText id="name" v-model="form.name" class="mt-1 w-full" :class="{ 'p-invalid': form.errors.name }" />
-                            <small v-if="form.errors.name" class="p-error text-xs">{{ form.errors.name }}</small>
-                        </div>
-                        <div>
-                            <label for="phone" class="block text-sm font-medium text-foreground">No. Telepon</label>
-                            <InputText id="phone" v-model="form.phone" class="mt-1 w-full" :class="{ 'p-invalid': form.errors.phone }" />
-                            <small v-if="form.errors.phone" class="p-error text-xs">{{ form.errors.phone }}</small>
-                        </div>
-                        <div>
-                            <label for="email" class="block text-sm font-medium text-foreground">Email</label>
-                            <InputText inputmode="email" id="email" v-model="form.email" class="mt-1 w-full" :class="{ 'p-invalid': form.errors.email }" />
-                            <small v-if="form.errors.email" class="p-error text-xs">{{ form.errors.email }}</small>
-                        </div>
-                        <div>
-                            <label for="password" class="block text-sm font-medium text-foreground">Password</label>
-                            <Password
-                                id="password"
-                                v-model="form.password"
-                                class="mt-1"
-                                :class="{ 'p-invalid': form.errors.password }"
-                                :feedback="true"
-                                min-length="8"
-                                toggleMask
-                            />
-                            <small v-if="form.errors.password" class="p-error text-xs">{{ form.errors.password }}</small>
-                        </div>
+        v-model:visible="dialogVisible"
+        modal
+        :header="dialogMode === 'add' ? 'Tambah Karyawan' : 'Ubah Karyawan'"
+        class="w-full max-w-lg sm:max-w-xl md:max-w-2xl"
+    >
+        <div class="flex flex-col gap-y-4 p-2 lg:p-4">
+            <div>
+                <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                    <div>
+                        <label for="name" class="text-foreground block text-sm font-medium">Nama</label>
+                        <InputText id="name" v-model="form.name" class="mt-1 w-full" :class="{ 'p-invalid': form.errors.name }" />
+                        <small v-if="form.errors.name" class="p-error text-xs">{{ form.errors.name }}</small>
+                    </div>
+                    <div>
+                        <label for="phone" class="text-foreground block text-sm font-medium">No. Telepon</label>
+                        <InputText id="phone" v-model="form.phone" class="mt-1 w-full" :class="{ 'p-invalid': form.errors.phone }" />
+                        <small v-if="form.errors.phone" class="p-error text-xs">{{ form.errors.phone }}</small>
+                    </div>
+                    <div>
+                        <label for="email" class="text-foreground block text-sm font-medium">Email</label>
+                        <InputText
+                            inputmode="email"
+                            id="email"
+                            v-model="form.email"
+                            class="mt-1 w-full"
+                            :class="{ 'p-invalid': form.errors.email }"
+                        />
+                        <small v-if="form.errors.email" class="p-error text-xs">{{ form.errors.email }}</small>
+                    </div>
+                    <div>
+                        <label for="password" class="text-foreground block text-sm font-medium">Password</label>
+                        <Password
+                            id="password"
+                            v-model="form.password"
+                            class="mt-1"
+                            :class="{ 'p-invalid': form.errors.password }"
+                            :feedback="true"
+                            min-length="8"
+                            toggleMask
+                        />
+                        <small v-if="form.errors.password" class="p-error text-xs">{{ form.errors.password }}</small>
                     </div>
                 </div>
             </div>
+        </div>
 
-            <template #footer>
-                <Button label="Batal" icon="pi pi-times" class="p-button-text p-button-secondary" @click="dialogVisible = false" />
-                <Button
-                    :label="dialogMode === 'add' ? 'Simpan' : 'Update'"
-                    icon="pi pi-check"
-                    class="p-button-success"
-                    @click="dialogMode === 'add' ? createUser() : updateUser()"
-                />
-            </template>
-        </Dialog>
+        <template #footer>
+            <Button label="Batal" icon="pi pi-times" class="p-button-text p-button-secondary" @click="dialogVisible = false" />
+            <Button
+                :label="dialogMode === 'add' ? 'Simpan' : 'Update'"
+                icon="pi pi-check"
+                class="p-button-success"
+                @click="dialogMode === 'add' ? createUser() : updateUser()"
+            />
+        </template>
+    </Dialog>
 
     <Dialog v-model:visible="displayDeleteDialog" modal header="Konfirmasi Hapus" :style="{ width: '450px' }">
         <div class="text-center">
