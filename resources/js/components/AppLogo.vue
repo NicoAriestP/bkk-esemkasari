c2ae7e503fdcbf1b8ced9642df3872d859f3ec20
NicoAriestP <nicoariestputra@gmail.com>
2025-06-13 16:57:28 +0700

    fix: style app logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,8 +3,8 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template class="!p-4">
-    <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+    <div class="flex aspect-square size-8 items-center justify-center rounded-md text-sidebar-primary-foreground">
+        <AppLogoIcon class="size-8 fill-current text-white dark:text-black" />
     </div>
     <div class="ml-1 grid flex-1 text-left">
         <span class="mb-0.5 text-md truncate font-semibold leading-none">BKK EDUSURYA</span>
